a443d606dee6e731e9b4547581017bccca497dec
Lars <k3larra@hotmail.com>
2013-08-30 15:54:21 +0200

    dadadadad

---
+++ Assignment1/src/se/mah/k3lara/assignment1/MainActivity.java
@@ -0,0 +1,33 @@
+package se.mah.k3lara.assignment1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void changeMe(View v){
+		Log.i("FirstApp","In on click");
+		TextView greenText = (TextView)findViewById(R.id.textView1);
+		greenText.setText("KooooooLLL");
+	}
+
+}
